94ed7f6a457bb8a4d987ee4420f2bde29004f34e
fredspiers <fredspiers@gmail.com>
2021-07-23 10:49:51 +0000

    initial commi

---
+++ resources/k3d-create-cluster.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+k3d cluster create fredcorp --image ixxel/k3s:v1.21.2-k3s1-alpine314 -p "5080:80@loadbalancer" -p "5443:443@loadbalancer" --volume "/home/fred/k3s-config/:/var/lib/rancher/k3s/server/manifests/"
